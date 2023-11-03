77ddb31a1f89f95ec6e90723c23f67f245e184e8
Kamran Ahmed <kamranahmed.se@gmail.com>
2023-11-03 15:16:41 +0000

    Format and add license

--- index.js
+++ index.js
@@ -1,40 +1,40 @@
 #!/usr/bin/env node
 
 if (!process.env.TOKEN || !process.env.CHANNEL) {
-    throw new Error('Please set the TOKEN and CHANNEL environment variables.');
+  throw new Error('Please set the TOKEN and CHANNEL environment variables.');
 }
 
 const slackToken = process.env.TOKEN;
 const slackChannel = process.env.CHANNEL;
 
 async function sendMessage(token, channel, message) {
-    return fetch('https://slack.com/api/chat.postMessage', {
-        method: 'post',
-        body: JSON.stringify({
-            channel,
-            blocks: [
-                {
-                    type: 'section',
-                    text: {
-                        type: 'mrkdwn',
-                        text: message,
-                    },
-                },
-            ],
-        }),
-        headers: {
-            'Content-Type': 'application/json; charset=utf-8',
-            Authorization: `Bearer ${token}`,
+  return fetch('https://slack.com/api/chat.postMessage', {
+    method: 'post',
+    body: JSON.stringify({
+      channel,
+      blocks: [
+        {
+          type: 'section',
+          text: {
+            type: 'mrkdwn',
+            text: message,
+          },
         },
-    });
+      ],
+    }),
+    headers: {
+      'Content-Type': 'application/json; charset=utf-8',
+      Authorization: `Bearer ${token}`,
+    },
+  });
 }
 
 // Usage: Pass the message as a command line argument
 if (process.argv.length < 3) {
-    console.error('Usage: slack-msg "Your message here"');
+  console.error('Usage: slack-msg "Your message here"');
 }
 
 const message = process.argv[2];
 sendMessage(slackToken, slackChannel, message)
-    .then(() => console.log('Message sent successfully'))
-    .catch((error) => console.error(error));
+  .then(() => console.log('Message sent successfully'))
+  .catch((error) => console.error(error));
